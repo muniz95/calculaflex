42f08984a66facea26aeed296b26066babf253a6
Rodrigo Muniz <muniz95@bsd.com.br>
2017-09-19 19:21:56 +0000

    Tema de cores alterado

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <Logo width="80" height="80" />
-          <h2>{`Welcome to Inferno ${version}`}</h2>
+          <h2>{`Welcome to CalculaFlex ${version}`}</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
