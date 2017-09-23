75bd451c07cbcdadd0778023181cf32b6746b557
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2017-09-23 02:37:41 -0300

    Remoção da máscara dos inputs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import Component from 'inferno-component';
 import Result from './Result.js';
 import './App.css';
-import masker from 'vanilla-masker'
-
 
 class App extends Component {
-	
-	maskValue = (value) => {
-		return masker.toMoney(value);
-	}
 
-  state = { result: '', highlight: 'none', alcohol: '0.00', gasoline: '0.00' };
+  state = { result: '', highlight: 'none', alcohol: undefined, gasoline: undefined };
 
 	setAlcohol = e => 
-		this.setState({ alcohol: (e.target.value < 0 ? 0 : this.maskValue(e.target.value)) })
+		this.setState({ alcohol: (e.target.value < 0 ? 0 : e.target.value) })
 	
 	setGasoline = e => 
-		this.setState({ gasoline: (e.target.value < 0 ? 0 : this.maskValue(e.target.value)) })
+		this.setState({ gasoline: (e.target.value < 0 ? 0 : e.target.value) })
 
 	parseValue = (value) => Number.parseFloat(value.replace(',', '.'))
 	
@@ -39,13 +33,13 @@ class App extends Component {
 		    </div>
 		    <p className="App-group">
 		      <label for="alcohol">Preço do álcool</label><br/>
-		      <input id="alcohol" type="text" className="App-input"
-						value={this.state.alcohol} onKeyUp={this.setAlcohol} /><br/>
+					<input id="alcohol" type="number" className="App-input"
+						value={this.state.alcohol} onChange={this.setAlcohol} /><br/>
 		    </p>
 		    <p className="App-group">
 		      <label for="gasoline">Preço da gasolina</label><br/>
-		      <input id="gasoline" type="text" className="App-input"
-						value={this.state.gasoline} onKeyUp={this.setGasoline} /><br/>
+		      <input id="gasoline" type="number" step="0.01" className="App-input"
+						value={this.state.gasoline} onChange={this.setGasoline} /><br/>
 		    </p>
 		    <p className="App-group">
 		      <button onClick={this.calculate}>Calcular</button>
